12d8b8efc85adb79c030bda5f683d6458569106f
amr <amr@amrs-Mac.local>
2017-12-16 13:38:18 -0800

    first page design without auto layout

---
+++ caramelloAppT1/ViewController2.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController2.swift
+//  caramelloAppT1
+//
+//  Created by amr on 12/16/17.
+//  Copyright © 2017 amr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+       // self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      //  self.navigationController?.navigationBar.shadowImage=UIImage()
+       // self.navigationController?.navigationBar.isTranslucent=true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+override var prefersStatusBarHidden: Bool
+    {
+    return true
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
